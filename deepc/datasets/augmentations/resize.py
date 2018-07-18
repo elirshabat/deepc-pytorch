63df23fd4872dc27bd52b24f33177d131da15cca
Shabat Eliran <es02877@elta.co.il>
2018-07-18 10:14:00 +0300

    add resize augmentation

--- deepc/datasets/augmentations/resize.py
+++ deepc/datasets/augmentations/resize.py
@@ -1,16 +1,34 @@
-import cv2
 import torch
+import numpy as np
+from PIL import Image
 
 
 class Resize:
 
     def __init__(self, h, w):
+        """
+        Construct an instance.
+        :param h: new height
+        :param w: new width
+        """
         self._h = h
         self._w = w
 
     def __call__(self, sample):
-        np_image = cv2.resize(sample['image'], dsize=(self._h, self._w), interpolation=cv2.INTER_NEAREST)
-        np_labels = cv2.resize(sample['labels'], dsize=(self._h, self._w), interpolation=cv2.INTER_NEAREST)
+        """
+        Resize the images in the given sample.
+        :param sample: Sample returned from dataset.
+        :return: The resized sample.
+        """
+        return {'image': self.resize_tensor_image(sample['image']),
+                'labels': self.resize_tensor_image(sample['labels'])}
 
-        return {'image': torch.from_numpy(np_image),
-                'labels': torch.from_numpy(np_labels)}
+    def resize_tensor_image(self, tensor):
+        """
+        Resize a tensor that represent an image.
+        """
+        original_np_img = np.array(tensor)
+        img = Image.fromarray(original_np_img).resize((self._w, self._h), Image.NEAREST)
+        np_img = np.array(img)
+        torch_img = torch.tensor(np_img, dtype=torch.uint8)
+        return torch_img
--- deepc/datasets/coco.py
+++ deepc/datasets/coco.py
@@ -2,10 +2,11 @@ from pycocotools.coco import COCO
 from torch.utils.data import Dataset
 import numpy as np
 import os.path
-import PIL.Image
+from PIL import Image
 import matplotlib.pyplot as plt
 import torch
 from augmentations.resize import Resize
+import yaml
 
 
 def show_sample(sample, ignore_image=False, ignore_labels=False):
@@ -15,9 +16,9 @@ def show_sample(sample, ignore_image=False, ignore_labels=False):
     """
     plt.figure()
     if not ignore_image:
-        plt.imshow(sample['image'].numpy())
+        plt.imshow(sample['image'])
     if not ignore_labels:
-        plt.imshow(sample['labels'].numpy(), alpha=0.5)
+        plt.imshow(sample['labels'], alpha=0.5)
 
 
 class CocoDataset(Dataset):
@@ -46,22 +47,36 @@ class CocoDataset(Dataset):
         img_id = self._img_ids[item]
         coco_img = self._coco.imgs[img_id]
         coco_anns = self._coco.loadAnns(self._coco.getAnnIds(imgIds=img_id))
-        labels = self._anns_to_ndarray(coco_img, coco_anns)
-        image_path = os.path.join(self._images_dir, coco_img['file_name'])
-        image = np.array(PIL.Image.open(image_path, 'r'))
 
-        sample = {'image': torch.from_numpy(image), 'labels': torch.from_numpy(labels)}
+        image_path = os.path.join(self._images_dir, coco_img['file_name'])
+        image = self.load_image(image_path)
+
+        labels = self._anns_to_tensor(coco_img, coco_anns)
+
+        sample = {'image': image, 'labels': labels}
         if self._transform:
             sample = self._transform(sample)
 
         return sample
 
-    def _anns_to_ndarray(self, img, anns):
+    @staticmethod
+    def load_image(file_path):
+        """
+        Load image from file to Tensor of type uint8.
+        :param file_path: path to image file
+        :return: Tensor that contains the image.
+        """
+        original_img = Image.open(file_path)
+        original_np_img = np.array(original_img)
+        original_torch_img = torch.tensor(original_np_img, dtype=torch.uint8)
+        return original_torch_img
+
+    def _anns_to_tensor(self, img, anns):
         """
         Convert coco annotations to numpy array that represent labels.
         :param img: Image dictionary in coco format.
         :param anns: Annotations of the given image.
-        :return: ndarray.
+        :return: Tensor.
         """
         image_size = (img['height'], img['width'])
         labels = np.zeros(image_size)
@@ -70,22 +85,25 @@ class CocoDataset(Dataset):
             label_mask = self._coco.annToMask(ann) == 1
             new_label = i + 1
             labels[label_mask] = new_label
-        return labels.astype('uint8')
+        return torch.tensor(labels.astype('uint8'), dtype=torch.uint8)
 
 
 if __name__ == '__main__':
 
-    normal_dataset = CocoDataset(r"E:\data\mlproj_dataset\coco\annotations\instances_train2014.json",
-                                 r"E:\data\mlproj_dataset\coco\images\train2014")
+    paths_config_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "Local", "paths.yaml")
+    with open(paths_config_file_path, 'r') as f:
+        paths = yaml.load(f)
+        img_dir = paths['coco_train2014_images']
+        anns_file = paths['coco_train2014_annotations']
 
-    resized_dataset = CocoDataset(r"E:\data\mlproj_dataset\coco\annotations\instances_train2014.json",
-                                  r"E:\data\mlproj_dataset\coco\images\train2014",
-                                  Resize(150, 200))
+    normal_dataset = CocoDataset(anns_file, img_dir)
+
+    resized_dataset = CocoDataset(anns_file, img_dir, Resize(200, 300))
 
     items = [0, 3, 17]
 
     for i in items:
-        show_sample(normal_dataset[i], ignore_labels=True)
-        show_sample(resized_dataset[i], ignore_labels=True)
+        show_sample(normal_dataset[i])
+        show_sample(resized_dataset[i])
 
     plt.show()
