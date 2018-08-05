dcacbdd95ac79218fe181f0fc634ec4aefa3ed31
Eliran Shabat <shabat.eliran@gmail.com>
2018-08-05 07:20:56 +0300

    add implementation notes

--- deepc/datasets/collate.py
+++ deepc/datasets/collate.py
@@ -15,3 +15,11 @@ def collate_fn(samples):
     #     end = lengths[i]
     #     targets[i, :end] = cap[:end]
     # return images, targets, lengths
+
+
+
+# stacked_images = torch.stack([s['image'] for s in samples])
+# stacked_labels = torch.stack([s['labels'] for s in samples])
+# max_clusters = max([len(s['cluster_ids']) for s in samples])
+# stacked_cluster_ids = torch.stack([torch.cat([s['cluster_ids'], -1*torch.ones(max_clusters - len(s['cluster_ids']), device=s['cluster_ids'].device, dtype=s['cluster_ids'].dtype)]) for s in samples])
+# out_sample = {'image': stacked_images, 'labels': stacked_labels, 'cluster_ids': stacked_cluster_ids}
