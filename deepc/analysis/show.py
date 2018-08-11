f4b3e59125076d76723cf222727f9841f083b831
Eliran Shabat <shabat.eliran@gmail.com>
2018-08-11 17:23:19 +0300

    add parallel coordinates plot

--- deepc/analysis/show.py
+++ deepc/analysis/show.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+import pandas
 
 
 def show_embeddings_3d(image_tensor, labels_tensor, y_tensor):
@@ -18,3 +19,16 @@ def show_embeddings_3d(image_tensor, labels_tensor, y_tensor):
         cluster_points = y[l == i]
         alpha = 0.1 if i == 0 else None
         ax.scatter(cluster_points[:, 0], cluster_points[:, 1], cluster_points[:, 2], alpha=alpha)
+
+
+def embeddings_parallel_coordinates(y_tensor, labels_tensor):
+    l, y = labels_tensor.numpy(), y_tensor.detach().numpy().transpose((1, 2, 0))
+    l, y = l.reshape(-1), y.reshape((-1, y.shape[2]))
+
+    n_dims = y.shape[1]
+
+    data = pandas.DataFrame(y, columns=[f"x{i}" for i in range(n_dims)])
+    data['label'] = l
+
+    plt.figure()
+    pandas.plotting.parallel_coordinates(data, 'label')
