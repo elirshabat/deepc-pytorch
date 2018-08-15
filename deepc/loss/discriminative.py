a123a09837f00ce15b1e44dadeb147780f4f0573
Eliran <shabat.eliran@gmail.com>
2018-08-15 20:04:35 +0300

    matrix version of loss + different hinge value for distance term

--- deepc/loss/discriminative.py
+++ deepc/loss/discriminative.py
@@ -42,110 +42,40 @@ class DiscriminativeLoss(torch.nn.Module):
         :return: the loss (scalar value)
         """
         batch_size, d, h, w = data.shape
+        var_terms, dist_terms, reg_terms = [], [], []
+
         for batch_index in range(batch_size):
             X, L = data[batch_index, :, :, :].view(d, -1), labels[batch_index, :, :].view(-1)
-            n = X.shape[1]
-            K = L.unique()
-            vars, mu = [], []
-            for k in K:
-                L_k = (L == k).unsqueeze(0).repeat([d, 1]).float()
-                X_k = X * L_k
-                mu_k = X_k.sum(1) / n
-                mu.append(mu_k)
-                var_k = (((X_k - mu_k.unsqueeze(1) * L_k).norm(dim=0) - self._delta_var).clamp(0) ** 2).sum() / n
-                vars.append(var_k)
+            cluster_ids = L.unique()
+            k = len(cluster_ids)
+            centers = []
 
-        MU = torch.stack(mu)
-        dist = [((self._delta_dist - (mu[k] - MU).norm(dim=1)).clamp(0) ** 2 / (len(K) - 1)).sum() / 2 for k in K]
+            if k <= 1:
+                var_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
+                dist_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
+                reg_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
+            else:
+                for c_index in range(k):
+                    c_id = cluster_ids[c_index]
+                    labels_mask = (L == c_id)
+                    c_size = labels_mask.sum().float()
+                    points_mask = labels_mask.unsqueeze(0).repeat([d, 1]).float()
+                    c_points = X*points_mask
+                    c_center = c_points.sum(1)/c_size
+                    centers.append(c_center)
+                    c_var = (((c_points - c_center.unsqueeze(1)*points_mask).norm(dim=0) - self._delta_var).clamp(0)**2).sum()/c_size
+                    var_terms.append(c_var)
 
-        # MU.norm(dim=1) #TODO: var term
-        #
-        # sum_term = sum([(loss_params['cluster_params'][c_id]['center'].norm() - np.sqrt(n_dims)).clamp(0)
-        #                 for c_id in loss_params['cluster_params']])
-        # sum_term / loss_params['num_clusters']
+                centers_tensor = torch.stack(centers)
+                for c_index in range(k):
+                    dist_term = ((self._delta_dist - (centers[c_index] - centers_tensor).norm(dim=1)).clamp(0)**2/(k*(k - 1))).sum()/2
+                    dist_terms.append(dist_term)
 
-        # batch_size = data.shape[0]
-        # var_terms, dist_terms, reg_terms = [], [], []
-        #
-        # for i in range(batch_size):
-        #
-        #     loss_params = self._calc_loss_params(data[i, :, :, :], labels[i, :, :])
-        #
-        #     if loss_params['num_clusters'] <= 1:
-        #         var_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
-        #         dist_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
-        #         reg_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
-        #     else:
-        #         var_terms.append(self._calc_var_term(loss_params))
-        #         dist_terms.append(self._calc_dist_term(loss_params))
-        #         reg_terms.append(self._calc_reg_term(loss_params, data.shape[1]))
-        #
-        # return (self._var_weight*sum(var_terms)
-        #         + self._dist_weight*sum(dist_terms)
-        #         + self._reg_weight*sum(reg_terms))/batch_size
+                reg_term = centers_tensor.norm(dim=1).mean()
+                reg_terms.append(reg_term)
 
-    @staticmethod
-    def _calc_loss_params(data, labels):
-        """
-        Calculate parameters needed to compute the loss.
-        :param data: input data points
-        :param labels: input labels
-        :return: loss parameters
-        """
-        out_params = dict()
-        out_params['cluster_ids'] = labels.unique()
-        out_params['num_clusters'] = len(out_params['cluster_ids'])
-        out_params['cluster_params'] = dict()
+        loss = (self._var_weight*torch.stack(var_terms).sum()
+                + self._dist_weight*torch.stack(dist_terms).sum()
+                + self._reg_weight*torch.stack(reg_terms).sum())/batch_size
 
-        for c_id in out_params['cluster_ids']:
-            c_params = dict()
-            c_params['mask'] = labels == c_id
-            c_params['size'] = c_params['mask'].sum().float()
-            rows = c_params['mask'].nonzero()[:, 0]
-            columns = c_params['mask'].nonzero()[:, 1]
-            c_params['points'] = data[:, rows, columns]
-            c_params['center'] = c_params['points'].sum(1) / c_params['size']
-
-            out_params['cluster_params'][c_id] = c_params
-
-        return out_params
-
-    def _calc_var_term(self, loss_params):
-        """
-        Calculate the variance term of the loss.
-        :param loss_params: parameters needed to compute the loss
-        :return: scalar value that is the variance term of the loss
-        """
-        c_vars = []
-        for c_id in loss_params['cluster_params']:
-            c_params = loss_params['cluster_params'][c_id]
-            center = c_params['center'].unsqueeze(1).repeat([1, c_params['points'].shape[1]])
-            c_var = (((c_params['points'] - center).norm(dim=0) - self._delta_var).clamp(0)**2).sum()
-            c_vars.append(c_var)
-
-        return sum(c_vars)/loss_params['num_clusters']
-
-    def _calc_dist_term(self, loss_params):
-        """
-        Calculate the distance term of the loss.
-        :param loss_params: parameters needed to compute the loss
-        :return: scalar value that is the distance term of the loss
-        """
-        centers = [loss_params['cluster_params'][c_id]['center'] for c_id in loss_params['cluster_params']]
-
-        sum_dist = sum([(2.0*self._delta_dist - (centers[i] - centers[j]).norm()).clamp(0)**2
-                        for i in range(len(centers))
-                        for j in range(len(centers))
-                        if i < j])
-
-        return sum_dist/(loss_params['num_clusters']*(loss_params['num_clusters'] - 1))
-
-    def _calc_reg_term(self, loss_params, n_dims):
-        """
-        Calculate the regularization term of the loss.
-        :param loss_params: parameters needed to compute the loss
-        :return: scalar value that is the regularization term of the loss
-        """
-        sum_term = sum([(loss_params['cluster_params'][c_id]['center'].norm() - np.sqrt(n_dims)).clamp(0)
-                        for c_id in loss_params['cluster_params']])
-        return sum_term/loss_params['num_clusters']
+        return loss
