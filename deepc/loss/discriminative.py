b046a2dd903499d3b70c1174e58316bbcf0d877d
Eliran <shabat.eliran@gmail.com>
2018-11-05 09:38:06 +0200

    vectorized computations inside mini-batch

--- deepc/loss/discriminative.py
+++ deepc/loss/discriminative.py
@@ -44,40 +44,49 @@ class DiscriminativeLoss(torch.nn.Module):
         delta_reg = self._delta_reg if self._delta_reg is not None else np.sqrt(d)
 
         for batch_index in range(batch_size):
+            # Compute batch information:
             X, L = data[batch_index, :, :, :].view(d, -1).permute(1, 0), labels[batch_index, :, :].view(-1)
             cluster_ids = L.unique()
             n_clusters = len(cluster_ids)
-            centers = []
 
+            # Handle the case of single instance (loss should be 0)
             if n_clusters <= 1:
                 var_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
                 dist_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
                 reg_terms.append(torch.tensor(0, device=data.device, dtype=torch.float))
+            # Handle the case of more than one instance
             else:
-                batch_var_terms = []
-                for c_index in range(n_clusters):
-                    labels_mask = (L == cluster_ids[c_index])
-                    pts = X[labels_mask]
-                    c_center = pts.mean(0)
-                    c_var = (((pts - c_center).norm(dim=1) - self._delta_var).clamp(0) ** 2).mean()
-                    centers.append(c_center)
-                    batch_var_terms.append(c_var)
-                var_term = torch.stack(batch_var_terms).mean()
+                # Compute the point-cluster indicator (n_clusters x n_points)
+                c_indicators = torch.stack([L == cluster_ids[c_index]
+                                            for c_index in range(n_clusters)]).type(torch.float32)
+                c_sizes = c_indicators.sum(1)
 
-                centers_tensor = torch.stack(centers)
+                # Compute the centers of the clusters (n_clusters x n_dims)
+                centers_tensor = torch.matmul(c_indicators, X) / c_sizes.unsqueeze(1)
+
+                # Compute the variance term of single example out of batch of examples
+                distance_from_centers = (X.unsqueeze(0) - centers_tensor.unsqueeze(1)).norm(dim=2)
+                distance_from_center_errors = (distance_from_centers - self._delta_var).clamp(0) ** 2
+                batch_var_terms = (c_indicators * distance_from_center_errors).sum(1) / c_sizes
+                var_term = batch_var_terms.mean()
+
+                # Compute the distance term of single example out of batch of examples
                 dist_matrix = (centers_tensor.unsqueeze(0) - centers_tensor.unsqueeze(1)).norm(dim=2)
-                dist_cost_matrix = ((self._delta_dist - (dist_matrix + torch.eye(n_clusters, device=data.device)*self._delta_dist)).clamp(0)**2)
-                dist_term = dist_cost_matrix.sum()/(n_clusters*(n_clusters - 1))
+                dist_cost_matrix = ((self._delta_dist - (
+                            dist_matrix + torch.eye(n_clusters, device=data.device) * self._delta_dist)).clamp(0) ** 2)
+                dist_term = dist_cost_matrix.sum() / (n_clusters * (n_clusters - 1))
 
-                reg_term = ((centers_tensor.norm(dim=1) - delta_reg).clamp(0)**2).mean()
+                # Compute the regularization term of single example out of batch of examples
+                reg_term = ((centers_tensor.norm(dim=1) - delta_reg).clamp(0) ** 2).mean()
 
+                # Save the computed terms for later use
                 var_terms.append(var_term)
                 dist_terms.append(dist_term)
                 reg_terms.append(reg_term)
 
-        loss = (self._var_weight*torch.stack(var_terms).mean()
-                + self._dist_weight*torch.stack(dist_terms).mean()
-                + self._reg_weight*torch.stack(reg_terms).mean())
+        loss = (self._var_weight * torch.stack(var_terms).mean()
+                + self._dist_weight * torch.stack(dist_terms).mean()
+                + self._reg_weight * torch.stack(reg_terms).mean())
 
         return loss
 
